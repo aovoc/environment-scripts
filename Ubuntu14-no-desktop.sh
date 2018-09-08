67e9fb23033c0daaa38bfa61164457a902a260d7
Minqin Chen <2295413340@qq.com>
2018-09-08 20:16:42 +0800

    Update Ubuntu14-no-desktop.sh

--- Ubuntu14-no-desktop.sh
+++ Ubuntu14-no-desktop.sh
@@ -75,7 +75,7 @@ pip_install_dependency opencv-python
 cd /train/results/
 git clone https://github.com/davisking/dlib.git
 cd dlib
-mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1; cmake --build . --config Release
 cd ..
 python setup.py install --yes USE_AVX_INSTRUCTIONS --yes DLIB_USE_CUDA
 mv ~/.vimrc ~/.vimrc_bak
