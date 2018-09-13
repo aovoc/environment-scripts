afaa1c279bdcace69a45de17e7ab32b2cf67fbdd
Minqin Chen <2295413340@qq.com>
2018-09-13 19:38:20 +0800

    Update Ubuntu14-no-desktop.sh

--- Ubuntu14-no-desktop.sh
+++ Ubuntu14-no-desktop.sh
@@ -92,8 +92,7 @@ rm -rf emacs-24.4
 sudo chmod -R 777 ~/.emacs.d
 sudo chmod -R 777 ~/.vimrc
 
-sudo apt-get install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
-
+#sudo apt-get install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 #opencv make config
 #cd opencv
 #mkdir release
