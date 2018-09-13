f33f745c081c4277d45e1fa3ef382c666671f498
Minqin Chen <2295413340@qq.com>
2018-09-13 19:46:01 +0800

    Update Ubuntu14-no-desktop.sh

--- Ubuntu14-no-desktop.sh
+++ Ubuntu14-no-desktop.sh
@@ -94,10 +94,9 @@ sudo chmod -R 777 ~/.vimrc
 
 #sudo apt-get install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 #opencv make config
-#cd opencv
+#cd opencv-2.4.13
 #mkdir release
 #cd release
 #cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=OFF -D WITH_OPENGL=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 -D WITH_CUBLAS=1 ..
 #make -j $(nproc)
 #sudo make install
-
